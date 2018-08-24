0f2111c320c5801447af4f53e48df027f8f0b239
Adam Schubert <adam.schubert@sg1-game.net>
2018-08-24 19:15:42 +0200

    New version 1.0.1

--- setup.py
+++ setup.py
@@ -39,7 +39,7 @@ extra_files = []
 
 setup(
     name='granad-gatekeeper',
-    version='1.0.0',
+    version='1.0.1',
     description='BESY GraNad Gatekeeper',
     long_description=open('README.md').read(),
     author='Adam Schubert',
