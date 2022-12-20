215dc93eb539281bfd195b729ff4bc56879e1776
Adam Schubert <adam.schubert@sg1-game.net>
2022-12-20 08:44:36 +0100

    New version 2.0.1

--- setup.py
+++ setup.py
@@ -30,7 +30,7 @@ classifiers = [s.strip() for s in classes.split('\n') if s]
 
 setup(
     name='netkeeper',
-    version='2.0.0',
+    version='2.0.1',
     description='Netkeeper keeps your huawei router connected',
     long_description=read_readme(),
     long_description_content_type='text/markdown',
